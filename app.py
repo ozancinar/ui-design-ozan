07496f5477845fa4c11822966d343d1dae045cb9
Jente Houweling <jente.houweling@rivm.nl>
2026-03-11 00:59:56 +0100

    refactor(data): removed empty line

--- app.py
+++ app.py
@@ -206,7 +206,6 @@ def data():
 
     # Extract datasets and metadata from Zenodo
     datasets = zen_result.get("hits", [])
-    
     zen_total = zen_result.get("total", 0)
     zen_error: str | None = zen_result.get("error", None)
 
